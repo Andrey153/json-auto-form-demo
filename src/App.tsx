991be956575fe9eba69d7148087378ca2600cedf
Andrey153 <vyalkov.a@gmail.com>
2023-04-10 13:44:25 +0300

    working demo version v0.1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { JSONAutoForm } from "json-auto-form";
+import example01 from "./example01.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <JSONAutoForm
+      inValue={example01}
+      style={{ position: "absolute", width: "100%", height: "100%" }}
+    />
   );
 }
 
