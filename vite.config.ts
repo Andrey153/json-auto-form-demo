c1478e3b8541e952171729e8cdbb5a268aa6a9f9
Andrey Vyalkov <vyalkov.a@gmail.com>
2023-04-16 15:53:41 +0300

    fix gh-pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  base: "./",
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 });
