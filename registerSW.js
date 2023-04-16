11265223edc81cd8ae73aa72e3e03e3d8cad3733
Andrey Vyalkov <vyalkov.a@gmail.com>
2023-04-16 15:02:35 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/vite-plugin-pwa' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '.' })})}
